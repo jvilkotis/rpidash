b45b8d28751a919601fc4bbabb46bb1bac0d7f8c
Justs <vilkotisjusts@gmail.com>
2024-03-12 23:11:18 +0200

    Javascript cleanup

--- rpidash/static/js/current-utilization.js
+++ rpidash/static/js/current-utilization.js
@@ -1,15 +1,17 @@
-function fetchCurrentUtilization() {
-  fetch("/services/current_utilization")
-    .then(response => response.json())
-    .then(data => {
-      displayCurrentUtilization(data);
-    })
-    .catch(error => {
-      console.error("There was a problem fetching the data:", error);
-    });
-}
-function displayCurrentUtilization(data) {
-  const {
+const fetchCurrentUtilization = async () => {
+  try {
+    const response = await fetch("/services/current_utilization");
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    displayCurrentUtilization(data);
+  } catch (error) {
+    console.error("There was a problem fetching the data:", error);
+  }
+};
+
+const displayCurrentUtilization = ({
   cpu_percentage,
   cpu_temperature,
   memory_used,
@@ -18,28 +20,27 @@ function displayCurrentUtilization(data) {
   storage_used,
   storage_percentage,
   storage_total
-  } = data;
-
+}) => {
   const cpuUtilizationElement = document.getElementById("current-cpu-utilization");
   const cpuTemperatureElement = document.getElementById("current-cpu-temperature");
   const memoryUtilizationElement = document.getElementById("current-memory-utilization");
   const storageUtilizationElement = document.getElementById("current-storage-utilization");
 
-  cpuUtilizationElement.innerHTML = `${cpu_percentage}%`;
-  cpuTemperatureElement.innerHTML = `${cpu_temperature} °C`;
-  memoryUtilizationElement.innerHTML = `${memory_used} MB (${memory_percentage}%) of ${memory_total} MB`;
-  storageUtilizationElement.innerHTML = `${storage_used} GB (${storage_percentage}%) of ${storage_total} GB`;
+  cpuUtilizationElement.textContent = `${cpu_percentage}%`;
+  cpuTemperatureElement.textContent = `${cpu_temperature} °C`;
+  memoryUtilizationElement.textContent = `${memory_used} MB (${memory_percentage}%) of ${memory_total} MB`;
+  storageUtilizationElement.textContent = `${storage_used} GB (${storage_percentage}%) of ${storage_total} GB`;
 
   const cpuUtilizationProgress = document.getElementById("cpu-utilization-progress");
   const cpuTemperatureProgress = document.getElementById("cpu-temperature-progress");
   const memoryUtilizationProgress = document.getElementById("memory-utilization-progress");
   const storageUtilizationProgress = document.getElementById("storage-utilization-progress");
 
-  cpuUtilizationProgress.style.width = 100 - cpu_percentage + "%";
-  cpuTemperatureProgress.style.width = 100 - cpu_temperature + "%";
-  memoryUtilizationProgress.style.width = 100 - memory_percentage + "%";
-  storageUtilizationProgress.style.width = 100 - storage_percentage + "%";
-}
+  cpuUtilizationProgress.style.width = `${100 - cpu_percentage}%`;
+  cpuTemperatureProgress.style.width = `${100 - cpu_temperature}%`;
+  memoryUtilizationProgress.style.width = `${100 - memory_percentage}%`;
+  storageUtilizationProgress.style.width = `${100 - storage_percentage}%`;
+};
 
 fetchCurrentUtilization();
 setInterval(fetchCurrentUtilization, 10000);
--- rpidash/static/js/graph.js
+++ rpidash/static/js/graph.js
@@ -1,41 +1,51 @@
-function fetchDataAndUpdateGraph(endpoint, id, valueKey, initializedFlag, index) {
-  fetch(endpoint)
-    .then(response => response.json())
-    .then(data => {
-      const dates = data.map(entry => new Date(entry.date));
-      const values = data.map(entry => parseFloat(entry[valueKey]));
-      const chartData = [{
-        x: dates,
-        y: values,
-        mode: "lines",
-        type: "scatter",
-        line: {color: "#B80C09"}
-      }];
-      const layout = {
-        xaxis: {zeroline: false},
-        yaxis: {zeroline: false},
-        dragmode: "pan",
-        margin: {t: 20}
-      };
-      const config = {
-        scrollZoom: true,
-        displayModeBar: false
-      };
-      if (!initializedFlag) {
-        Plotly.newPlot(id, chartData, layout, config);
-        graphs[index].initializedFlag = true;
-      } else {
-        const updatedData = {
-          x: [dates],
-          y: [values]
-        };
-        Plotly.update(id, updatedData);
-      }
-    })
-    .catch(error => {
-      console.error("There was a problem fetching the data:", error);
-    });
-}
+const fetchDataAndUpdateGraph = async (graph) => {
+  try {
+    const response = await fetch(graph.endpoint);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+
+    const dates = data.map(entry => new Date(entry.date));
+    const values = data.map(entry => parseFloat(entry[graph.valueKey]));
+    const chartData = [{
+      x: dates,
+      y: values,
+      mode: "lines",
+      type: "scatter",
+      line: { color: "#B80C09" }
+    }];
+
+    const layout = {
+      xaxis: { zeroline: false },
+      yaxis: { zeroline: false },
+      dragmode: "pan",
+      margin: { t: 20 }
+    };
+
+    const config = {
+      scrollZoom: true,
+      displayModeBar: false
+    };
+
+    if (!graph.initializedFlag) {
+      Plotly.newPlot(graph.id, chartData, layout, config);
+      graph.initializedFlag = true;
+    } else {
+      const updatedData = { x: [dates], y: [values] };
+      Plotly.update(graph.id, updatedData);
+    }
+  } catch (error) {
+    console.error("There was a problem fetching the data:", error);
+  }
+};
+
+const initializeGraphs = async () => {
+  for (const graph of graphs) {
+    await fetchDataAndUpdateGraph(graph);
+    setInterval(() => fetchDataAndUpdateGraph(graph), 10000);
+  }
+};
 
 const graphs = [
   {
@@ -58,22 +68,4 @@ const graphs = [
   }
 ];
 
-graphs.forEach((graph, index) => {
-  fetchDataAndUpdateGraph(
-    graph.endpoint,
-    graph.id,
-    graph.valueKey,
-    graph.initializedFlag,
-    index
-  );
-
-  setInterval(() => {
-    fetchDataAndUpdateGraph(
-      graph.endpoint,
-      graph.id,
-      graph.valueKey,
-      graph.initializedFlag,
-      index
-    );
-  }, 10000);
-});
+initializeGraphs();
